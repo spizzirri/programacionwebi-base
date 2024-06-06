306fb19439b128b484cb9f9bf4133d6a1e7fb7db
damian spizzirri <damianspizzirri@gmail.com>
2024-06-05 22:06:06 -0300

    Muestra como leer y guardar datos en el localstorage

--- 2024/05-junio/scripts/serie.js
+++ 2024/05-junio/scripts/serie.js
@@ -1,8 +1,15 @@
-const QUERY_PARAMS_NOMBRE = 'nombre';
+const QUERY_PARAMS_NOMBRE = 'indice';
 const url_vista = document.location.href;
 const url = new URL(url_vista)
-const nombre_de_la_serie = url.searchParams.get(QUERY_PARAMS_NOMBRE)
+const indice_de_la_serie = url.searchParams.get(QUERY_PARAMS_NOMBRE)
 
-const nodo_texto = document.createTextNode(nombre_de_la_serie)
+const SERIES = JSON.parse(localStorage.getItem('series'))
+
+const nodo_texto_h1 = document.createTextNode(SERIES[indice_de_la_serie]['nombre'])
 const nodo_h1 = document.querySelector('h1')
-nodo_h1.appendChild(nodo_texto)
+nodo_h1.appendChild(nodo_texto_h1)
+
+
+const nodo_texto_p = document.createTextNode(SERIES[indice_de_la_serie]['sinopsis'])
+const nodo_p = document.querySelector('p')
+nodo_p.appendChild(nodo_texto_p)
