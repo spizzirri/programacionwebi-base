d72b3613cf3f83041b1b9a2908fe6c37741686d0
damian spizzirri <damianspizzirri@gmail.com>
2024-06-05 22:15:34 -0300

    Implementa una clase para simplificar codigo

--- 2024/05-junio/scripts/serie.js
+++ 2024/05-junio/scripts/serie.js
@@ -5,11 +5,13 @@ const indice_de_la_serie = url.searchParams.get(QUERY_PARAMS_NOMBRE)
 
 const SERIES = JSON.parse(localStorage.getItem('series'))
 
-const nodo_texto_h1 = document.createTextNode(SERIES[indice_de_la_serie]['nombre'])
+const serie = new Serie(SERIES[indice_de_la_serie])
+
+const nodo_texto_h1 = document.createTextNode(serie.nombre)
 const nodo_h1 = document.querySelector('h1')
 nodo_h1.appendChild(nodo_texto_h1)
 
 
-const nodo_texto_p = document.createTextNode(SERIES[indice_de_la_serie]['sinopsis'])
+const nodo_texto_p = document.createTextNode(serie.sinopsis)
 const nodo_p = document.querySelector('p')
 nodo_p.appendChild(nodo_texto_p)
