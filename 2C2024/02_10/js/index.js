53db3f109d47ff094b15db34c1b66f72b9827c50
David Alaniz <davidalaniz@MacBook-Pro-de-David.local>
2024-10-08 11:36:50 -0300

    02 octubre - Js

---
+++ 2C2024/02_10/js/index.js
@@ -0,0 +1,23 @@
+console.log("Hola Mundo!");
+
+// var , let, const , ES6
+
+/* let saludar = "dice hola";
+let tiempos = 4;
+
+if (tiempos > 3) {
+  const hola = "dice Hola tambien";
+  console.log(hola);
+}
+
+console.log(hola); */
+
+// var , let, const , ES6
+
+// let userName = prompt("Cual es tu nombre");
+
+// Concatenacion
+// alert("Hola" + userName + "Bienvenido a la pagina" + " " + "de la tienda");
+
+// Template Literals
+// alert(`Hola ${userName} Bienvenido a la pagina de la tienda.`);
