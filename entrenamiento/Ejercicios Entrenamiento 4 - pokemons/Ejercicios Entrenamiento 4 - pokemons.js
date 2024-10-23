d72cb10ae90a2097bf6c6fca345b80b92777f1b6
damian spizzirri <damianspizzirri@gmail.com>
2024-10-23 16:37:14 -0300

    Agregar ejercicio de pokemon

---
+++ entrenamiento/Ejercicios Entrenamiento 4 - pokemons/Ejercicios Entrenamiento 4 - pokemons.js
@@ -0,0 +1,83 @@
+[
+  {
+    "nombre": "Bulbasaur",
+    "poderes": [
+      {
+        "nombre": "Placaje",
+        "energia": 10,
+        "usos": 4
+      },
+      {
+        "nombre": "Látigo Cepa",
+        "energia": 15,
+        "usos": 3
+      },
+      {
+        "nombre": "Hoja Afilada",
+        "energia": 20,
+        "usos": 3
+      }
+    ]
+  },
+  {
+    "nombre": "Charmander",
+    "poderes": [
+      {
+        "nombre": "Arañazo",
+        "energia": 10,
+        "usos": 4
+      },
+      {
+        "nombre": "Lanzallamas",
+        "energia": 30,
+        "usos": 2
+      },
+      {
+        "nombre": "Dragoaliento",
+        "energia": 25,
+        "usos": 3
+      }
+    ]
+  },
+  {
+    "nombre": "Squirtle",
+    "poderes": [
+      {
+        "nombre": "Pistola Agua",
+        "energia": 15,
+        "usos": 4
+      },
+      {
+        "nombre": "Burbuja",
+        "energia": 10,
+        "usos": 4
+      },
+      {
+        "nombre": "Placaje",
+        "energia": 10,
+        "usos": 2
+      }
+    ]
+  },
+  {
+    "nombre": "Pikachu",
+    "poderes": [
+      {
+        "nombre": "Impactrueno",
+        "energia": 20,
+        "usos": 3
+      },
+      {
+        "nombre": "Ataque Rápido",
+        "energia": 10,
+        "usos": 4
+      },
+      {
+        "nombre": "Bola Voltio",
+        "energia": 25,
+        "usos": 2
+      }
+    ]
+  }
+]
+
