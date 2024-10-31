8163483a084162b5bfe7d979ddb2ec500f1ef985
damian spizzirri <damianspizzirri@gmail.com>
2024-10-30 21:53:46 -0300

    Validaciones con el localstorage

--- 2C2024/30_10/script.js
+++ 2C2024/30_10/script.js
@@ -7,26 +7,62 @@ function validarFormulario(evento){
     nodoError.innerHTML = "";
     const nodoUsuario = document.querySelector("#usuario");
     const nodoClave = document.querySelector("#clave");
+    const nodoEmail = document.querySelector("#email");
 
-    if(nodoUsuario.value.length != 8 )
+    if(nodoUsuario.value.length != 3 )
         nodoError.innerHTML += "<p>El usuario debe tener 8 caracteres</p>"
+    if(!validarQueElUsuarioNoExiste(nodoUsuario.value)){
+        nodoError.innerHTML += "<p>El usuario ya existe</p>"
+    }
 
     // tenga 8 caracteres, 4 sean numeros y 4 sean letras
-    if(nodoClave.value.length != 8){
-        nodoError.innerHTML += "<p>La clave debe tener 8 caracteres, 4 numero y 4 letras</p>"
+    if(nodoClave.value.length != 3){
+        nodoError.innerHTML += "<p>La clave debe tener 3 caracteres, 1 numero y 2 letras</p>"
     }else{
         if(!validarClave(nodoClave.value)){
-            nodoError.innerHTML += "<p>La clave debe tener 4 numero y 4 letras</p>"
+            nodoError.innerHTML += "<p>La clave debe tener 1 numero y 2 letras</p>"
         }
     }
 
-    if(!nodoError.innerHTML)
+    /**
+     * [ {...}, {...}, {...} ]
+     * { usuario1: {}, usuario2: {},   }
+     * 
+     */
+    if(!nodoError.innerHTML){
+        let usuariosRegistrados = {}
+        const usuariosRegistradosJSON = localStorage.getItem('usuarios');
+        if(usuariosRegistradosJSON){
+            usuariosRegistrados = JSON.parse(usuariosRegistradosJSON)
+            usuariosRegistrados[nodoUsuario.value] = { 
+                usuario: nodoUsuario.value,
+                clave: nodoClave.value,
+                email: nodoEmail.value
+             }
+        }else{
+            usuariosRegistrados[nodoUsuario.value] = {
+                usuario: nodoUsuario.value,
+                clave: nodoClave.value,
+                email: nodoEmail.value
+            }
+        }
+        localStorage.setItem("usuarios", JSON.stringify(usuariosRegistrados))
         nodoForm.submit()
+    }
+        
+}
+
+function validarQueElUsuarioNoExiste(usuario){
+    const usuariosJSON = localStorage.getItem('usuarios');
+    if(!usuariosJSON)
+        return true;
+    const usuarios = JSON.parse(usuariosJSON);
+    return !usuarios.hasOwnProperty(usuario)
 }
 
 function validarClave(clave){
     // Hacer una logica para que la clave tenga 4 numero y 4 letras
-    return false;    
+    return true;
 }
 
 
