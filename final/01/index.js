8f06c86f1c2df2579b3ef0484902e2671da0a454
damian spizzirri <damianspizzirri@gmail.com>
2025-10-29 20:35:39 -0300

    final01 - primera version

---
+++ final/01/index.js
@@ -0,0 +1,41 @@
+const botones = document.querySelectorAll('button');
+const resultado = document.querySelector('p');
+
+const VALIDACION = 'Validacion: '
+let operacion = '';
+let cantidadDeClick = 0;
+let numero1 = '';
+let numero2 = '';
+let operador = '';
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+
+        cantidadDeClick++;
+        if(cantidadDeClick == 1) numero1 = boton.textContent;
+        if(cantidadDeClick == 2) operador = boton.textContent;
+        if(cantidadDeClick == 3) numero2 = boton.textContent;
+
+        operacion += boton.textContent;
+        resultado.textContent = `${VALIDACION} ${operacion}`;
+        
+
+        if(cantidadDeClick == 3){
+
+            switch(operador){
+                case '<':
+                    resultado.textContent = `${VALIDACION} ${operacion} = ${Number(numero1) < Number(numero2)}`;
+                    break;
+                case '>':
+                    resultado.textContent = `${VALIDACION} ${operacion} = ${Number(numero1) > Number(numero2)}`;
+                    break;
+                case '=':
+                    resultado.textContent = `${VALIDACION} ${operacion} = ${Number(numero1) === Number(numero2)}`;
+                    break;
+                case '!=':
+                    resultado.textContent = `${VALIDACION} ${operacion} = ${Number(numero1) !== Number(numero2)}`;
+                    break;
+            }
+        }
+    });
+});
